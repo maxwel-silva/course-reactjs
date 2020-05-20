e558938d0e9b135e0b60daef47c2ead3e97a3ee2
maxwel-silva <maxwel.santos4@etec.sp.gov.br>
2020-05-20 00:07:29 -0300

    pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import './styles.css'
 
-import Header from './components/header/index'
+import Header from './components/header/index.js'
+import Main from './pages/main/index.js'
 
 const App = () => (
   <div className="App">
+    
     <Header />
+    <Main />
+    
   </div>
 )
 
---
+++ src/pages/main/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+import api from '../../services/api.js'
+
+export default class Main extends Component {
+  state = {
+    products: []
+  }
+
+  componentDidMount() {
+    this.loadProducts()
+  }
+
+  loadProducts = async () => {
+    const response = await api.get('/products')
+
+    this.setState({ products: response.data.docs })
+  }
+
+  render() {
+    return (
+      <div className="product-list">{
+
+        this.state.products.map(product => (
+          <h3 key={product._id}>{product.title}</h3>
+        ))
+      }
+      </div>
+    )
+  }
+}
